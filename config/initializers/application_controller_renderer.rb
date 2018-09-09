1e50e07c087e87ab809a59dc7c5ce9ea726af1a3
141naoto <naot.ishii141@gmail.com>
2018-09-09 12:06:35 +0900

    finish 3-9

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'b8cd57b482c27a0503c4e30f06289ac8096e3614e87cb4241921610987dec61d74ee615be6a67814fed2963cc01ad94b1293a12bd1ae81924037b01ca8241b09'
